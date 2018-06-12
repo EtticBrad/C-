648941cc9a78de917e6c305ca38b513a50341c31
U-BSD\462356 <462356@SHS-10DCE0A2A.beaverton.k12.or.us>
2018-06-12 12:02:47 -0700

    HI

--- add.cpp
+++ add.cpp
@@ -2,11 +2,25 @@
 #include"math.h"
 using namespace std;
 
+void vol();
+
+void area();
+
 void basic();
 
 int main() {
-  cout <<"Basic math (1), Area (2), Volume(3)"
+  int t;
   
+  cout <<"Basic math (1), Area (2), Volume(3)" <<endl;
+  cin >> t;
+  
+  if (t == 1) {
+    basic();
+  } else if (t == 2) {
+    area();
+  } else if (t == 3) {
+    vol();
+  }
 }
 
 void basic() {
@@ -16,7 +30,7 @@ void basic() {
   double z;
   
   cout <<"Enter an integer please." <<endl;
-  cin >> x;
+  cin >> y;
 
   cout <<endl;
 
@@ -38,5 +52,68 @@ void basic() {
     z = sqrt((y*y)+(x*x));
   }
 
-  cout <<endl <<z <<endl;
+  cout <<endl <<z;
+}
+
+void area() {
+  double x;
+  double y;
+  int t;
+  double z;
+
+  cout <<"Rectangle (1), Cicle (2), Triangle (3)" <<endl;
+  cin >> t;
+
+  if (t == 1) {
+    cout <<"Enter length." <<endl;
+    cin >> x;
+
+    cout <<endl;
+
+    cout <<"Enter width." <<endl;
+    cin >> y;
+
+    z = x * y;
+  } else if (t == 2) {
+    cout <<"Enter radius" <<endl;
+    cin >> x;
+
+    z = 3.14159 * (x * x);
+  } else if (t == 3) {
+    cout <<"Enter base" <<endl;
+    cin >> x;
+
+    cout <<"Enter height" <<endl;
+    cin >> y;
+    
+    z = 0.5 * y * x;
+  }
+
+  cout <<endl <<z;
+}
+
+void vol() {
+  double x;
+  double y;
+  double j;
+  int t;
+  double z;
+
+  cout <<"Prisim (1)" <<endl;
+  cin >> t;
+
+  if (t == 1) {
+    cout <<"Enter width" <<endl;
+    cin >> x;
+
+    cout <<"Enter length" <<endl;
+    cin >> y;
+
+    cout <<"Enter height" <<endl;
+    cin >> j;
+
+    z = x * y * j;
+  }
+
+  cout <<endl <<z;
 }
